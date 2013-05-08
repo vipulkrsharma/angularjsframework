b1d24d4a7f933d12da4a583c44e778795750ac9d
ashish-sharma-manusis <ashish.sharma@manusis.com>
2013-05-08 16:13:50 +0530

    minor changes

---
+++ src/js/stackoverflowcontroller.js
@@ -0,0 +1,6 @@
+function stackoverflowCtrl($scope, $http)
+{
+        $http.jsonp("http://api.stackoverflow.com/1.1/search?intitle=angularjs&sort=votes&jsonp=JSON_CALLBACK").success(function(data){
+            $scope.stackoverflowData = data;
+        });
+}
